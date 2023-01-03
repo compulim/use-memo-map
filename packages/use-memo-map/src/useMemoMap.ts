a318462f3d00df8cf6110ab328f43e7ecdf6539d
William Wong <compulim@hotmail.com>
2023-01-02 22:41:28 -0800

    Use use-ref-from

--- packages/use-memo-map/src/useMemoMap.ts
+++ packages/use-memo-map/src/useMemoMap.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef } from 'react';
+import { useRefFrom } from 'use-ref-from';
 
 import usePrevious from './private/usePrevious';
-import useValueRef from './private/useValueRef';
 
 type UseMemoMapOptions<T> = {
   itemEquality?: (this: readonly T[], x: T, y: T) => boolean;
@@ -21,9 +21,9 @@ export default function useMemoMap<T = unknown, R = unknown>(
   mapper: (this: readonly T[], item: T, index: -1, array: readonly T[]) => R,
   { itemEquality = Object.is }: UseMemoMapOptions<T> = {}
 ): (array: readonly T[]) => readonly R[] {
-  const itemEqualityRef = useValueRef(itemEquality);
+  const itemEqualityRef = useRefFrom(itemEquality);
   const lastCallsRef = useRef<readonly [T, R][]>([]);
-  const mapperRef = useValueRef(mapper);
+  const mapperRef = useRefFrom(mapper);
   const thisCalls: [T, R][] = [];
 
   if (usePrevious(mapper) !== mapper) {
@@ -34,7 +34,7 @@ export default function useMemoMap<T = unknown, R = unknown>(
     lastCallsRef.current = Object.freeze(thisCalls);
   });
 
-  const thisCallsRef = useValueRef(thisCalls);
+  const thisCallsRef = useRefFrom(thisCalls);
 
   return useCallback<(array: readonly T[]) => readonly R[]>(
     (array: readonly T[]) => {
