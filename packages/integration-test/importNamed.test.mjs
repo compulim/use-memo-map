fcddc356de6167737ba78dc751c0e280cfc20c7e
William Wong <compulim@users.noreply.github.com>
2023-01-03 01:14:45 -0800

    Add tests and clean up issue template (#3)
    * Add tests
    
    * Update description

---
+++ packages/integration-test/importNamed.test.mjs
@@ -0,0 +1,17 @@
+/** @jest-environment jsdom */
+
+import { renderHook } from '@testing-library/react-hooks';
+import useMemoMap from 'use-memo-map/useMemoMap';
+
+test('simple scenario', () => {
+  // GIVEN: A "multiply by 10" mapper.
+  const mapper = x => x * 10;
+
+  // WHEN: Calling with input of [1, 2, 3].
+  const { result } = renderHook(({ input }) => useMemoMap(mapper)(input), {
+    initialProps: { input: [1, 2, 3] }
+  });
+
+  // THEN: It should return [10, 20, 30].
+  expect(result.current).toEqual([10, 20, 30]);
+});
