1ddfb287bc9d1794a6e4ecaed08fd984366d875a
William Wong <compulim@hotmail.com>
2023-03-21 03:53:36 -0700

    Update template

---
+++ packages/pages/src/App.tsx
@@ -0,0 +1,25 @@
+import React, { useCallback } from 'react';
+import { useMemoMap } from 'use-memo-map';
+
+const App = () => {
+  const multiplyBy10 = useCallback<(value: number) => number>(value => {
+    // Calls to this function will be memoized based on its first argument.
+    // You can do expensive calls here.
+    return value * 10;
+  }, []);
+
+  // useMemoMap() will return a function that take an array.
+  const map = useMemoMap(multiplyBy10);
+
+  const output = map([1, 2, 3]); // Returns [10, 20, 30].
+
+  return (
+    <ul>
+      {output.map(value => (
+        <li>{value}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default App;
